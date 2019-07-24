0319a31814c6854f46f0feb664abce4c0b47dd09
maddiestrange <maddie.strange@gmail.com>
2019-07-24 15:48:41 -0500

    built out Form.js functionality and passing useState array to App.js, now rendering cards from imput data

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form'
 
 function App() {
+  const teamArray = [
+    {
+    name: 'Maddie',
+    role: 'Front-End',
+    email: 'maddie.strange@gmail.com'
+    }
+  ]
+  const [team, setTeam] = useState(teamArray);
+  console.log(team);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form member={team} setMember={setTeam}/>
+      {team.map((member, index) => (
+        <div className='member-card' key={index}>
+          <h3>Name: {member.name}</h3>
+          <h4>Role: {member.role}</h4>
+          <p>Email: {member.email}</p>
+        </div>
+      ))}
     </div>
   );
 }
